a32bddd85cf2221907c8dad6e3309f80cfdaa3af
fadiyaldo <61246362+fadiyaldo@users.noreply.github.com>
2021-06-04 00:54:05 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Data from "./Data";
+import List from "./List";
+function App() {
+  const [people, setPeople] = useState(Data);
+  return (
+    <main>
+      <section className="container">
+        <h3> {people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}> you clicked me</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
